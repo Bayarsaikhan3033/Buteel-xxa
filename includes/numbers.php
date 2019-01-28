d1ce6faca2d828ef02e4e28cca461ed2e86d22a6
Bayarsaikhan <https://github.com/Bayarsaikhan3033>
2019-01-28 15:01:53 +0800

    includes (functions, errors, messages ....)

---
+++ includes/numbers.php
@@ -0,0 +1,38 @@
+	<div id="numbers" class="section sm-padding">
+		<div class="bg-img" style="background-image: url('./img/background2.jpg');">
+			<div class="overlay"></div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4 col-xs-6">
+					<div class="number">
+						<i class="fa fa-users"></i>
+						<h3 class="white-text"><span class="counter">
+			<?php echo $user_too; ?>
+						</span></h3>
+						<span class="white-text">Хэрэглэгч</span>
+					</div>
+				</div>
+				<div class="col-sm-4 col-xs-6">
+					<div class="number">
+						<i class="fa fa-question-circle" aria-hidden="true"></i>
+						<h3 class="white-text"><span class="counter">
+			<?php echo $post_too; ?>
+						</span></h3>
+						<span class="white-text">Нийтлэл</span>
+					</div>
+				</div>
+				<div class="col-sm-4 col-xs-6">
+					<div class="number">
+						<i class="fa fa-pencil-square" aria-hidden="true"></i>
+						<h3 class="white-text"><span class="counter">
+			<?php echo $comment_too; ?>	
+						</span></h3>
+						<span class="white-text">Сэтгэгдэл</span>
+					</div>
+				</div>
+				<!-- /number -->
+
+			</div>
+		</div>
+	</div>
